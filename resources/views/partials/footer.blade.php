e016a4cd28e8e6dca0be66deab015f15384f93a4
nasir03 <allfinnasir@gmail.com>
2025-06-20 06:13:00 +0700

    first commit

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,46 @@
+<!-- Footer -->
+<footer class="sticky-footer bg-white">
+  <div class="container my-auto">
+    <div class="copyright text-center my-auto">
+      <span>copyright &copy; <script> document.write(new Date().getFullYear()); </script> - developed by
+        <b><a href="https://indrijunanda.gitlab.io/" target="_blank">indrijunanda</a></b>
+      </span>
+    </div>
+  </div>
+
+  <div class="container my-auto py-2">
+    <div class="copyright text-center my-auto">
+      <span>copyright &copy; <script> document.write(new Date().getFullYear()); </script> - distributed by
+        <b><a href="https://themewagon.com/" target="_blank">themewagon</a></b>
+      </span>
+    </div>
+  </div>
+</footer>
+<!-- Footer -->
+
+<!-- Scroll to top -->
+<a class="scroll-to-top rounded" href="#page-top">
+  <i class="fas fa-angle-up"></i>
+</a>
+
+<!-- Modal Logout -->
+<div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabelLogout"
+  aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabelLogout">Ohh No!</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <p>Are you sure you want to logout?</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-outline-primary" data-dismiss="modal">Cancel</button>
+        <a href="login.html" class="btn btn-primary">Logout</a>
+      </div>
+    </div>
+  </div>
+</div>
---
+++ resources/views/partials/scripts.blade.php
@@ -0,0 +1,10 @@
+<script src="{{ asset('back-end/vendor/jquery/jquery.min.js') }}"></script>
+<script src="{{ asset('back-end/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('back-end/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+<script src="{{ asset('back-end/js/ruang-admin.min.js') }}"></script>
+<script src="{{ asset('back-end/vendor/chart.js/Chart.min.js') }}"></script>
+<script src="{{ asset('back-end/js/demo/chart-area-demo.js') }}"></script>
+
+</body>
+
+</html>
